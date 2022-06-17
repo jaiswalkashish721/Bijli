b8cebff53d8fe41132fe2cf37b2b1a00c16bfe34
Kashish <manish.p.jaiswal@gmail.com>
2022-06-17 17:43:23 +0530

    k

--- src/bijli/Login.java
+++ src/bijli/Login.java
@@ -212,7 +212,7 @@ public class Login extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        tf1.setText(""+meter_no);
+        tf1.setText("");
         pf1.setText("");
     }//GEN-LAST:event_jButton1ActionPerformed
 
